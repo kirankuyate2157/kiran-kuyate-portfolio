33cc4fce19e4541563cae5c7ca27dca12a6215e2
Kiran kuyate <84271800+kirankuyate2157@users.noreply.github.com>
2023-04-12 18:33:29 +0530

    nav added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,27 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import Head from "next/head";
+import type { AppProps } from "next/app";
+import { Montserrat } from "next/font/google";
+import Navbar from "../components/Navbar";
+const montserrat = Montserrat({
+  subsets: ["latin"],
+  variable: "--font-mont",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>my portfolio</title>
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <main
+        className={`${montserrat.variable} font-mont bg-light w-full min-h-screen`}
+      >
+        <Navbar />
+        <Component {...pageProps} />
+      </main>
+    </>
+  );
 }
