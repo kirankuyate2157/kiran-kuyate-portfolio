7f4c41d5c1cea5477f7c5b057e0d7633375dc24e
Kiran kuyate <84271800+kirankuyate2157@users.noreply.github.com>
2023-08-20 23:38:01 +0530

    21 pj

--- src/pages/about.js
+++ src/pages/about.js
@@ -107,7 +107,7 @@ const About = () => {
               </div>
               <div className='flex flex-col items-end justify-center xl:items-center'>
                 <span className='inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl'>
-                  <AnimatedNumber value={30} />+
+                  <AnimatedNumber value={21} />+
                 </span>
                 <h2 className='text-xl font-medium capitalize text-dark/75  dark:text-light/75 xl:text-center md:text-lg sm:text-base xs:text-sm'>
                   Projects completed
